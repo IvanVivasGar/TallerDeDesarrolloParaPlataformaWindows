dc40d18ef57f8679e8230f9620682f1e47ff9b13
Ivan Vivas Garcia <ivg77850@lasallebajio.edu.mx>
2024-04-20 19:15:47 -0600

    Ejercicio 3 Actualizacion

--- TDMPW_2P_EJ03/obj/Debug/net8.0-ios/iossimulator-x64/TDMPW_2P_EJ03.AssemblyInfo.cs
+++ TDMPW_2P_EJ03/obj/Debug/net8.0-ios/iossimulator-x64/TDMPW_2P_EJ03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TDMPW_2P_EJ03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+aad3e58f37c0dbcc12faa0bc866a26fa84e208e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d2a28d62445a4a7eed46d86fe841bb66416f2fa9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TDMPW_2P_EJ03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TDMPW_2P_EJ03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
