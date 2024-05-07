8e60dec5dd3c4eb3517244443d164c2679c47fbe
Ivan Vivas Garcia <ivg77850@lasallebajio.edu.mx>
2024-05-07 14:17:07 -0600

    Pagina 1 y 2 terminada

--- TDMPW_2P_EX_77850/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/TDMPW_2P_EX_77850.AssemblyInfo.cs
+++ TDMPW_2P_EX_77850/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/TDMPW_2P_EX_77850.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TDMPW_2P_EX_77850")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c93a7de7e867ad7006c7945dcd813a3881025739")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a1b4f59b063658f76a4158e1fee329ece2d69b0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TDMPW_2P_EX_77850")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TDMPW_2P_EX_77850")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
