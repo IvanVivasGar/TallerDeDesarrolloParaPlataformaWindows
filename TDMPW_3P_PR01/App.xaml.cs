5b27854708dd5a4752e202a21eb4f994603c6557
Ivan Vivas Garcia <ivg77850@lasallebajio.edu.mx>
2024-05-15 22:43:00 -0600

    Proyecto 01 P03

---
+++ TDMPW_3P_PR01/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.WindowsAppSDK.Runtime.Packages;
+
+namespace TDMPW_3P_PR01;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
