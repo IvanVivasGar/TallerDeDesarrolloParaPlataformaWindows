c8db5feaebcb894cd6f3e2b86ff4f18d02543623
Ivan Vivas Garcia <ivg77850@lasallebajio.edu.mx>
2024-05-29 23:24:35 -0600

    Ejercicio 02
    Ejercicio 02 del tercer parcial, incuye fondo degradado

--- TDMPW_3P_PR02/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/TDMPW_3P_PR02.AssemblyInfo.cs
+++ TDMPW_3P_PR02/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/TDMPW_3P_PR02.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TDMPW_3P_PR02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4b1f4515fda004a08f1362338c3248b26c094a73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fe122df504d157556bbb390e206eeb0a086cde57")]
 [assembly: System.Reflection.AssemblyProductAttribute("TDMPW_3P_PR02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TDMPW_3P_PR02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
