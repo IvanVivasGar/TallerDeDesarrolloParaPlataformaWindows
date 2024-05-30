81c54a586e48758e226508454c646a9586afdf2f
Ivan Vivas Garcia <ivg77850@lasallebajio.edu.mx>
2024-05-30 00:37:43 -0600

    Eliminacion PR03

--- TDMPW_3P_PR03/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace TDMPW_3P_PR03;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
