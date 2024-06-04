7ec8b956832b96cef3c8b33722bee10f1c238c04
Ivan Vivas Garcia <ivg77850@lasallebajio.edu.mx>
2024-06-04 11:58:11 -0600

    Creacion EJ03

--- TDMPW_3P_PR03/obj/Debug/net8.0-ios/iossimulator-x64/TDMPW_3P_PR03.AssemblyInfo.cs
+++ TDMPW_3P_PR03/obj/Debug/net8.0-ios/iossimulator-x64/TDMPW_3P_PR03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TDMPW_3P_PR03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b6b5e0fa91dafa94522326ee480e89f073d7027a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+25930f33eec5592bd1475eb787acafe20a23aa68")]
 [assembly: System.Reflection.AssemblyProductAttribute("TDMPW_3P_PR03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TDMPW_3P_PR03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
