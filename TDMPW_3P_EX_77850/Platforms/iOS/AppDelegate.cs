edcb3735265739b15dd4b0fae4627279c4c57ad6
Ivan Vivas Garcia <ivg77850@lasallebajio.edu.mx>
2024-06-08 18:45:16 -0600

    Rehacer Examen

--- TDMPW_3P_EX_77850/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace TDMPW_3P_EX_77850;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
